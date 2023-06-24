fee4573691ca0d0e7a6060d211dafd957c65fb8c
StellarPhantasm30 <adityame0010@gmail.com>
2023-06-24 08:25:24 +0530

    Practice directory with greedy algo questions package added.

---
+++ Practice/src/com/greedy/MinimumPlatforms.java
@@ -0,0 +1,22 @@
+package com.greedy;
+
+/*
+Problem Statement:  We are given two arrays that represent the arrival and departure times of trains that stop at the platform.
+We need to find the minimum number of platforms needed at the railway station so that no train has to wait.
+
+Input: N=6, 
+arr[] = {900, 945, 955, 1100, 1500, 1800} 
+dep[] = {920, 1200, 1130, 1150, 1900, 2000}
+
+Output:3
+
+Link: https://takeuforward.org/data-structure/minimum-number-of-platforms-required-for-a-railway/
+*/
+
+public class MinimumPlatforms {
+
+	public static void main(String[] args) {
+
+	}
+
+}
